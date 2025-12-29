83a7b6200a5fdb42b4b45ab6950c382d06604bc6
José Naves <josenaves@gmail.com>
2025-12-29 11:08:04 -0300

    feat: Add Jundiaí vs Sorocaba battle video composition with new data and assets.

--- src/Root.tsx
+++ src/Root.tsx
@@ -11,6 +11,8 @@ import arceburgoGuaranesiaData from "./data/arceburgo-guaranesia.json";
 // @ts-ignore
 import saoJoseRioPardoMococaData from "./data/sao-jose-rio-pardo-mococa.json";
 // @ts-ignore
+import jundiaiSorocabaData from "./data/jundiai-sorocaba.json";
+// @ts-ignore
 import extremaPousoAlegreData from "./data/extrema-pouso-alegre.json";
 // @ts-ignore
 import bauruRibeiraoPretoData from "./data/bauru-ribeirao-preto.json";
@@ -88,6 +90,20 @@ export const RemotionRoot: React.FC = () => {
           battleData: saoJoseRioPardoMococaData,
           image1: "sao-jose-do-rio-pardo.png",
           image2: "mococa.jpg",
+        }}
+      />
+
+      <Composition
+        id="BattleJundiaiSorocaba"
+        component={BattleVideo}
+        durationInFrames={1200}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          battleData: jundiaiSorocabaData,
+          image1: "jundiai.jpg",
+          image2: "sorocaba.webp",
         }}
       />
 
