cde551e41cfcecf3aadeaa7bc7f82dbf8590b0b4
José Naves <josenaves@gmail.com>
2026-01-02 11:56:45 -0300

    feat: add Barueri vs Carapicuíba

--- src/Root.tsx
+++ src/Root.tsx
@@ -43,6 +43,8 @@ import barueriItapeviData from "./data/barueri-itapevi.json";
 // @ts-ignore
 import barueriSaoCaetanoData from "./data/barueri-sao-caetano-do-sul.json";
 // @ts-ignore
+import barueriCarapicuibaData from "./data/barueri-carapicuiba.json";
+// @ts-ignore
 import portoUniaoUniaoDaVitoriaData from "./data/porto-uniao-uniao-da-vitoria.json";
 // @ts-ignore
 import mococaParaisoData from "./data/mococa-paraiso.json";
@@ -340,6 +342,20 @@ export const RemotionRoot: React.FC = () => {
           battleData: barueriSaoCaetanoData,
           image1: "barueri.jpg",
           image2: "sao-caetano-do-sul.jpeg",
+        }}
+      />
+
+      <Composition
+        id="BattleBarueriCarapicuiba"
+        component={BattleVideo}
+        durationInFrames={1200}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          battleData: barueriCarapicuibaData,
+          image1: "barueri.jpg",
+          image2: "carapicuiba.jpg",
         }}
       />
       <Composition
